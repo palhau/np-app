f6d8d21f11eca442c0ac261b56d3927010cdb4e8
Alan Vieira Palhau <75443906+alan-palhau@users.noreply.github.com>
2021-03-23 06:53:18 -0300

    feature: add publish fuction and route

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
-import prisma from '../../lib/prisma';
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import ReactMarkdown from 'react-markdown'
+import Layout from '../../components/Layout'
+import Router from 'next/router'
+import { PostProps } from '../../components/Post'
+import { useSession } from 'next-auth/client'
+import prisma from '../../lib/prisma'
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -12,14 +14,29 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   })
-  return { props: post }
+  return {
+    props: post,
+  }
+}
+
+async function publishPost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/publish/${id}`, {
+    method: 'PUT',
+  })
+  await Router.push('/')
 }
 
 const Post: React.FC<PostProps> = (props) => {
+  const [session, loading] = useSession()
+  if (loading) {
+    return <div>Authenticating ...</div>
+  }
+  const userHasValidSession = Boolean(session)
+  const postBelongsToUser = session?.user?.email === props.author?.email
   let title = props.title
   if (!props.published) {
     title = `${title} (Draft)`
@@ -29,8 +46,11 @@ const Post: React.FC<PostProps> = (props) => {
     <Layout>
       <div>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
+        <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown source={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
       </div>
       <style jsx>{`
         .page {
